e13b8de7d896cf5e08e48b737b09e716a6876b7f
Nikita Grebnev <81284413+grubnev@users.noreply.github.com>
2024-01-17 13:15:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from transformers import pipeline
 model_name = "deepset/roberta-base-squad2"
 QA_model = pipeline('question-answering', model=model_name, tokenizer=model_name)
 def main():
-    st.title("Question Answering with Hugging Face Model")
-    input_text = st.text_input("Enter your text", "")
-    input_question = st.text_input("Enter your question", "")
-    if st.button("Answer"):
+    st.title("Ответ на вопрос по тексту при помощи модели Roberta-base :blue[(Hugging Face Model)] :hugging_face:")
+    st.image("Leonardo_Diffusion_XL_Question_Answering_with_Hugging_Face.jpg", use_column_width=True)
+    input_text = st.text_input("Скопируйте сюда Ваш текст", "")
+    input_question = st.text_input("Введите Ваш вопрос", "")
+    if st.button("Получить ответ"):
         answer = QA_model({
             'question': input_question,
             'context': input_text,
         })
-        st.write('Answer:', answer['answer'])
+        st.write("Ответ:", answer['answer'])
 
 if __name__ == '__main__':
     main()
